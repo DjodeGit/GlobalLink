f04d1920c35e89205d69ce6b125330d448de7d30
ortegaeditor <ortegaeditor@gmail.com>
2026-02-27 11:23:30 +0200

    fix :UtilisateurNonConnecte/Login/Signup/FilActualite

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    # Pas de champs supplémentaires pour l'instant (on reste minimal)
+    # On pourra ajouter bio, photo, etc. plus tard si besoin
+
+    def __str__(self):
+        return self.username
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('feed')
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, 'registration/signup.html', {'form': form})
