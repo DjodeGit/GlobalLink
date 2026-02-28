cec25616d8d27bb910938b4f1e268c76cfd7f3f0
Tenang <medonjiojodeanas@gmail.com>
2026-02-28 22:42:06 +0100

    fix:modification du fichier models.py provenant de l application posts

--- posts/models.py
+++ posts/models.py
@@ -7,9 +7,15 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='liked_posts', blank=True)
 
     def __str__(self):
         return f"Post de {self.author.username} - {self.created_at.strftime('%d/%m/%Y')}"
+
+    @property
+    def total_likes(self):
+        """Retourne le nombre de likes sur ce post"""
+        return self.likes.count()
 
     class Meta:
         ordering = ['-created_at']
