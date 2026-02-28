8517c7a73374b6a0e631b0d03cde66bd6daf3d44
Tenang <medonjiojodeanas@gmail.com>
2026-02-28 22:40:53 +0100

    fix:modification du fichier models.py provenant de l application interactions

--- interactions/models.py
+++ interactions/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.conf import settings
 from posts.models import Post
 
+
+class CommentLike(models.Model):
+    """Modèle pour liker les commentaires"""
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    comment = models.ForeignKey('Comment', on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'comment')
+
+
 class Comment(models.Model):
     post = models.ForeignKey('posts.Post', on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -13,6 +24,11 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Commentaire de {self.author.username} sur {self.post.id}"
+    
+    @property
+    def total_likes(self):
+        """Retourne le nombre de likes sur ce commentaire"""
+        return self.likes.count()
     
 
 
